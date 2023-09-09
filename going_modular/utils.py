e0b8d3d18b3c06e78046465dbb8925d0f462e704
Ewan Chamberlain <ewanachamberlain@gmail.com>
2023-09-09 22:02:00 +0100

    finished 06, completed up to middle of 07.4

--- going_modular/utils.py
+++ going_modular/utils.py
@@ -2,7 +2,10 @@
 Contains various utility functions for PyTorch model training and saving.
 """
 import torch
+import torchvision
+import matplotlib.pyplot as plt
 from pathlib import Path
+from typing import List
 
 def save_model(model: torch.nn.Module,
                target_dir: str,
@@ -34,3 +37,46 @@ def save_model(model: torch.nn.Module,
     print(f'[INFO] Saving model to: {model_save_path}')
     torch.save(obj=[model.kwargs, model.state_dict()],
                f=model_save_path)
+
+def pred_and_plot_image(model: torch.nn.Module,
+                        image_path: str,
+                        device: torch.device,
+                        class_names: List[str] = None,
+                        transform=None):
+    """Makes a prediction on a target image and plots the image with its prediction."""
+    # Load in image and convert the tensor values to float32
+    target_image = torchvision.io.read_image(str(image_path)).type(torch.float32)
+
+    # Divide the image pixel values by 255 to get them between [0, 1]
+    target_image = target_image / 255.
+
+    # Transform if necessary
+    if transform:
+        target_image = transform(target_image)
+
+    # Make sure the model is on the target device
+    model.to(device)
+
+    # Turn on model evaluation mode and inference mode
+    model.eval()
+    with torch.inference_mode():
+        # Add an extra dimension to the image
+        target_image = target_image.unsqueeze(dim=0)
+
+        # Make a prediction on image with an extra dimension and send it to the target device
+        target_image_pred = model(target_image.to(device))
+
+    # Convert logits -> prediction probabilities
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # Covert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
+    # Plot the image alongside the prediction and prediction probability
+    plt.imshow(target_image.squeeze().permute(1, 2, 0)) # make sure it's the right size for Matplotlib
+    if class_names:
+        title = f'Pred: {class_names[target_image_pred_label.cpu()]} | Prob: {target_image_pred_probs.max().cpu():.3f}'
+    else:
+        title = f'Pred: {target_image_pred_label} | Prob: {target_image_pred_probs.max().cpu():.3f}'
+    plt.title(title)
+    plt.axis(False);
